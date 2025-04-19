7dd513310dcccdf90e1435c960ea487ad06d3555
yeet77827 <lukegailitis@gmail.com>
2025-04-18 22:04:05 -0400

    Add files via upload
    Original Version 
    https://github.com/nathanielxd/simple-lan-chat
    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
    Based on the original program with a few additional features 
    
    has a built in chat importer and chat exporter feature as well a light dark mode feature that you customize the color if you want to change it
    the chat itself now has a display when someone is typing as well as a 24 hour clock assigned to each message as well as the online marker
    ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------

--- src/chatapplication/ChatApp.java
+++ src/chatapplication/ChatApp.java
@@ -43,25 +43,41 @@ public class ChatApp extends javax.swing.JFrame {
         jButton3 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setBackground(new java.awt.Color(255, 255, 153));
+        getContentPane().setBackground(new java.awt.Color(240, 24, 24));
         setFont(new java.awt.Font("Cambria Math", 3, 10)); // NOI18N
-        setForeground(new java.awt.Color(255, 0, 0));
+        setForeground(new java.awt.Color(240, 0, 0));
 
-        jButton2.setFont(new java.awt.Font("Gabriola", 1, 24)); // NOI18N
+        jButton2.setFont(new java.awt.Font("Times New Roman", 1, 24)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(255, 255, 255)); // Set text color to white
+        jButton2.setBackground(new java.awt.Color(0, 128, 0)); // Set background color to green
         jButton2.setText("Login to Chat Room");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
+            @Override
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
-
         jButton3.setFont(new java.awt.Font("Gabriola", 1, 24)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(0, 0, 0)); // Set text color to red
+        jButton3.setBackground(new java.awt.Color(0, 89, 192)); // Set background color to light gray
         jButton3.setText("Exit");
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton3ActionPerformed(evt);
+            jButton3ActionPerformed(evt);
             }
         });
+
+        jButton4 = new javax.swing.JButton();
+        jButton4.setFont(new java.awt.Font("Gabriola", 1, 24)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(255, 255, 255)); // Set text color to white
+        jButton4.setBackground(new java.awt.Color(128, 0, 128)); // Set background color to purple
+        jButton4.setText("Insert File");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            jButton4ActionPerformed(evt);
+            }
+        });
+
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -103,6 +119,11 @@ public class ChatApp extends javax.swing.JFrame {
        System.exit(1);
     }//GEN-LAST:event_jButton3ActionPerformed
 
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        // TODO add your handling code here:
+        System.out.println("Insert File button clicked");
+    }//GEN-LAST:event_jButton4ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -141,5 +162,6 @@ public class ChatApp extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
     // End of variables declaration//GEN-END:variables
 }
